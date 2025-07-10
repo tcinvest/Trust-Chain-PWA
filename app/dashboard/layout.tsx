8c590f5c306f3ec9b5db53c28043663a820faa96
kiwachirobert <kiwackirobert783@gmail.com>
2025-07-10 18:48:46 +0000

    ggg

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,8 +8,8 @@ export default async function DashboardLayout({
 }) {
   await checkUser()
   return (
-    <div className="flex flex-col h-screen">
-      <main className="flex-1 overflow-y-auto pb-20 relative z-10">
+    <div className="flex flex-col h-screen bg-white dark:bg-slate-900">
+      <main className="flex-1 overflow-y-auto bg-white dark:bg-slate-900 pb-20 relative z-10">
         {children}
       </main>
       <Nav />
