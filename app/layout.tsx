d085c1d255f908af8da1ab2db9500342f9b80899
kiwachirobert <kiwackirobert783@gmail.com>
2025-07-17 13:58:15 +0000

    fddgs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <Analytics/>
       <html lang="en" suppressHydrationWarning><body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
