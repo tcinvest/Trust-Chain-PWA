159a032f10c62ea0ff3fa3323cce36a01ad1b692
tcinvest <kiwackirobert783@gmail.com>
2026-05-11 14:24:01 +0000

    Create GoogleTranslate.tsx

---
+++ components/GoogleTranslate.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { useEffect } from 'react';
+
+declare global {
+interface Window {
+  googleTranslateInit: () => void;
+  google: {
+    translate: {
+      translateElement: new (
+        options: { pageLanguage: string; autoDisplay: boolean },
+        element: string
+        ) => void;
+    };
+  }
+
+  export default function GoogleTranslate() {
+    useEffect(() => {
+      if (document.getElementById('google-translate-script')) return;
+
+      window.googleTranslateInit = () => {
+        new window.google.translate.TranslateElement(
+          { pageLanguage: 'en', autoDisplay: false },
+          'google_translate_element'
+          );
+      };
+
+      const script = document.createElement('script');
+      script.id = 'google-translate-script';
+      script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateInit';
+      script.async = true;
+      document.body.appendChild(script);
+
+      return () => {
+        const existingScript = document.getElementById('google-translate-script');
+        if (existingScript) document.body.removeChild(exisingScript);
+      };
+    }, []);
+
+    return <div id="google_translate_element" />
+  };
